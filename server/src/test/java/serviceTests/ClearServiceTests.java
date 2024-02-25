de2ce49770e04f78c0143e70d61ad2fb32504e91
Miles Spencer <milesspencer35@gmail.com>
2024-02-24 20:38:45 -0700

    Update ClearServiceTests to work with GameServiceTests

--- server/src/test/java/serviceTests/ClearServiceTests.java
+++ server/src/test/java/serviceTests/ClearServiceTests.java
@@ -25,16 +25,16 @@ public class ClearServiceTests {
     public void testClearApplication() {
         try {
             int game1 = gameDAO.createGame("myGame");
-            int game2 = gameDAO.createGame("myGame");
-            Assertions.assertEquals(2, gameDAO.listGames().size(), "Assert number of games == 2");
+            int game2 = gameDAO.createGame("myGame2");
+            Assertions.assertNotEquals(0, gameDAO.listGames().size(), "Assert there are games");
 
             userDAO.createUser("Miles", "Hello123", "miles@gmail.com");
             userDAO.createUser("Jamal", "BYU123", "jamal@gmail.com");
-            Assertions.assertEquals(2, userDAO.numberOfUsers(), "Assert number of Users == 2");
+            Assertions.assertNotEquals(0, userDAO.numberOfUsers(), "Assert there are Users");
 
             authDAO.createAuth("Miles");
             authDAO.createAuth("Jamal");
-            Assertions.assertEquals(2, authDAO.numberOfAuths(), "Assert number of Auths == 2");
+            Assertions.assertNotEquals(0, authDAO.numberOfAuths(), "Assert there are Auths");
 
             clearService.clearApplication();
             Assertions.assertEquals(0, gameDAO.listGames().size(), "Assert number of games == 0");
