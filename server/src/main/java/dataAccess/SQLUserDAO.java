e718eabd921be333f65ace586718161fb26b28c0
Miles Spencer <milesspencer35@gmail.com>
2024-03-05 20:41:53 -0700

    Start implementing SQLUserDAO and Make JDBC driver available

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -2,23 +2,64 @@ package dataAccess;
 
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SQLUserDAO extends DAO implements UserDAO{
     public SQLUserDAO() throws DataAccessException {
     }
 
     @Override
     public void clear() throws DataAccessException {
+        String sql = "DELETE FROM user";
 
+        Connection connection = null;
+        try (Connection c = DatabaseManager.getConnection()) {
+            connection = c;
+            try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        String sql = "SELECT username, password, email FROM users WHERE username = " + username;
+
+        Connection connection = null;
+        try (Connection c = DatabaseManager.getConnection()) {
+            connection = c;
+            try(PreparedStatement stmt = connection.prepareStatement(sql); ResultSet rs = stmt.executeQuery()) {
+                String rsUsername = rs.getString(1);
+                String rsPassword = rs.getString(2);
+                String rsEmail = rs.getString(3);
+                return new UserData(rsUsername, rsPassword, rsEmail);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
     @Override
     public void createUser(String username, String password, String email) throws DataAccessException {
+        String sql = "insert into user (username, password, email) values (? ,? ,?) ";
 
+        Connection connection = null;
+        try (Connection c = DatabaseManager.getConnection()) {
+            connection = c;
+            try(PreparedStatement stmt = connection.prepareStatement(sql); ResultSet rs = stmt.executeQuery()) {
+                stmt.setString(1, username);
+                stmt.setString(2, password);
+                stmt.setString(3, email);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
     @Override
