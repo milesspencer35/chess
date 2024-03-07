2f0ddcf9c1302658d43c83de7210f7500bcb37f8
Miles Spencer <milesspencer35@gmail.com>
2024-03-07 11:03:33 -0700

    Implement clear in SQLAuthDAO

--- server/src/main/java/dataAccess/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -2,13 +2,27 @@ package dataAccess;
 
 import model.AuthData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SQLAuthDAO extends DAO implements AuthDAO {
     public SQLAuthDAO() throws DataAccessException {
     }
 
     @Override
     public void clear() throws DataAccessException {
+        String sql = "DELETE FROM auth";
 
+        Connection connection = null;
+        try (Connection c = DatabaseManager.getConnection()) {
+            connection = c;
+            try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
     @Override
