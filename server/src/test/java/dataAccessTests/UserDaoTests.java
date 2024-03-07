fdec83a67f63c2d27a07fe612dfa543ec3794255
Miles Spencer <milesspencer35@gmail.com>
2024-03-06 21:26:37 -0700

    Add positive and negative test for creating a user

--- server/src/test/java/dataAccessTests/UserDaoTests.java
+++ server/src/test/java/dataAccessTests/UserDaoTests.java
@@ -1,11 +1,42 @@
 package dataAccessTests;
 
+import chess.InvalidMoveException;
+import dataAccess.DataAccessException;
+import dataAccess.SQLUserDAO;
+import dataAccess.UserDAO;
+import model.UserData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserDaoTests {
-    @Test
-    public void clearTest( ) {
+    UserDAO userDAO = new SQLUserDAO();
 
+    public UserDaoTests() throws DataAccessException {
+    }
+
+    @AfterEach
+    public void clearDatabase() throws DataAccessException {
+        userDAO.clear();
+    }
+
+
+    @Test
+   public void createUserCorrectly() throws DataAccessException {
+       userDAO.createUser("Johnny", "johnny123", "johnny@gmail.com");
+       UserData user = userDAO.getUser("Johnny");
+
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("Johnny", user.username());
+        Assertions.assertEquals("johnny123", user.password());
+        Assertions.assertEquals("johnny@gmail.com", user.email());
+    }
+
+    @Test
+    public void createUserMissingInformation() throws DataAccessException {
+        Assertions.assertThrows(DataAccessException.class,
+                () -> userDAO.createUser("Ryan", null, "ryan@gmail.com" ));
     }
 
     @Test
@@ -13,6 +44,9 @@ public class UserDaoTests {
 
     }
 
+    @Test
+    public void clearTest( ) {
 
+    }
 
 }
