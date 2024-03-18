0d02bc88daf0379eae0df2a784131b97707bff7d
Miles Spencer <milesspencer35@gmail.com>
2024-03-18 11:53:30 -0600

    Contiue implementing drawing the chess board

--- client/src/main/java/ui/DrawChessBoard.java
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -10,8 +10,16 @@ import static ui.EscapeSequences.*;
 
 public class DrawChessBoard {
 
+    private static final int BOARD_WIDTH = 8;
+    private static final int BOARD_HEIGHT = 8;
+
     public static void main(String[] args) {
-        drawBoard();
+        ChessBoard board = new ChessBoard();
+        board.resetBoard();
+        ChessGame game = new ChessGame();
+        game.setBoard(board);
+
+        drawBoard(game);
     }
 
     public static void drawBoard(ChessGame game) {
@@ -20,22 +28,67 @@ public class DrawChessBoard {
         out.print(ERASE_SCREEN);
 
         drawLetterCoordinates(out, game.getTeamTurn());
+        drawPlaySpace(out, game.getBoard());
+        drawLetterCoordinates(out, game.getTeamTurn());
     }
 
     private static void drawLetterCoordinates(PrintStream out, ChessGame.TeamColor color) {
         String[] whiteLetterCoordinates = {"a", "b", "c", "d", "e", "f", "g", "h"};
         String[] blackLetterCoordinates = {"h", "g", "f", "e", "d", "c", "b", "a"};
-
         String[] letterCoordinates =
                 color == ChessGame.TeamColor.WHITE ? whiteLetterCoordinates : blackLetterCoordinates;
 
-        // start here
+        setBackgroundGray(out);
+        drawEMPTY(out);
+        for (int boardCol = 0; boardCol < BOARD_WIDTH; boardCol++) {
+            drawLetter(out, letterCoordinates[boardCol]);
+        }
+        drawEMPTY(out);
 
+        resetBGColor(out);
+        out.println();
     }
 
-    private static void setBlack(PrintStream out) {
-        out.print(SET_BG_COLOR_BLACK);
+    private static void drawLetter(PrintStream out, String letterText) {
+        out.print(" ");
+        out.print(letterText);
+        out.print(" ");
+    }
+
+    private static void drawPlaySpace(PrintStream out, ChessBoard board) {
+        for (int boardRow = 1; boardRow <= BOARD_HEIGHT; boardRow++) {
+            drawNumberCoordinate(out, boardRow);
+
+
+
+
+            drawNumberCoordinate(out, boardRow);
+            resetBGColor(out);
+            out.println();
+        }
+    }
+
+    private static void drawNumberCoordinate(PrintStream out, int boardRow) {
+        setBackgroundGray(out);
+        out.print(" ");
+        out.print(boardRow);
+        out.print(" ");
+    }
+
+
+
+
+    private static void drawEMPTY(PrintStream out) {
+        out.print(EMPTY);
+    }
+
+    private static void setBackgroundGray(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
+    private static void resetBGColor(PrintStream out) {
+        out.print("\u001B[0m");
+
+    }
 }
