adac5fe238c14c28de0a4cc943542818548ed765
Miles Spencer <milesspencer35@gmail.com>
2024-03-21 20:11:50 -0600

    Start implementing serverfacade and add register test to serverFacade tests

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,2 +1,90 @@
-package client;public class ServerFacade {
+package client;
+
+import com.google.gson.Gson;
+import model.AuthData;
+import model.ErrorData;
+import model.UserData;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URI;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public AuthData register(String username, String password, String email) throws ResponseException{
+        UserData user = new UserData(username, password, email);
+        var path = "/user";
+        return this.makeRequest("POST", path, user, AuthData.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            // Sets url
+            URL url = (new URI(serverUrl + path)).toURL();
+            // opens http connection
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            // sets the type of request (GET, POST,...)
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            // adds the body to http request
+            writeBody(request, http);
+            http.connect();
+            // throw error if not successful
+            throwIfNotSuccessful(http);
+            // read response body and return it
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream errorBody = http.getErrorStream()) {
+                InputStreamReader reader = new InputStreamReader(errorBody);
+                ErrorData errorMessage = new Gson().fromJson(reader, ErrorData.class);
+                throw new ResponseException(status, "failure: " + status + " " + errorMessage.message());
+            }
+        }
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,5 +1,6 @@
 package clientTests;
 
+import client.ServerFacade;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -7,12 +8,15 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        var url = "http://localhost:" + port;
+        facade = new ServerFacade(url);
     }
 
     @AfterAll
@@ -20,10 +24,10 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void register() throws Exception {
+        var authData = facade.register("player2", "password", "p1@email.com");
+        Assertions.assertTrue(authData.authToken().length() > 10);
     }
 
 }
