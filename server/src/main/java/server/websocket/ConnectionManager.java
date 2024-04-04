19e2f699f2208c950e101d898a58e846f0682fcd
Miles Spencer <milesspencer35@gmail.com>
2024-04-04 11:20:50 -0600

    Start implementing ConnectionManager.java in Server

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,2 +1,40 @@
-package server.websocket;public class ConnectionManager {
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    // ConcurrentHashMap is multi thread safe
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String authToken, Session session) {
+        var connection = new Connection(authToken, session);
+        connections.put(authToken, connection);
+    }
+
+    public void remove(String authToken) {
+        connections.remove(authToken);
+    }
+
+    public void broadcast(String excludeAuthToken, ServerMessage serverMessage) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.authToken.equals(excludeAuthToken)) {
+                    c.send(serverMessage.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.authToken);
+        }
+    }
 }
