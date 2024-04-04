9236ef634bce7b803def02c375648fc4b20d7e6b
Miles Spencer <milesspencer35@gmail.com>
2024-04-03 21:16:45 -0600

    Create WebsocketCommunicator and start implementing it

---
+++ client/src/main/java/client/WebsocketCommunicator.java
@@ -0,0 +1,51 @@
+package client;
+import com.google.gson.Gson;
+import exception.ResponseException;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketCommunicator extends Endpoint {
+    private Session session;
+    private ServerMessageObserver serverMessageObserver;
+
+    public WebsocketCommunicator(String url, ServerMessageObserver serverMessageObserver) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/connect");
+            this.serverMessageObserver = serverMessageObserver;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    serverMessageObserver.notify(serverMessage);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    // TODO: implement something like this for each of the UserGameCommands
+//    public void enterPetShop(String visitorName) throws ResponseException {
+//        try {
+//            var action = new Action(Action.Type.ENTER, visitorName);
+//            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+//        } catch (IOException ex) {
+//            throw new ResponseException(500, ex.getMessage());
+//        }
+//    }
+
+}
