cc6a5cdef7a9784ab0d9e87ecacebbc6eb0c445f
Miles Spencer <milesspencer35@gmail.com>
2024-04-10 21:32:27 -0600

    Add ErrorMessage, LoadGameMessage, and NotificationMessage classes

---
+++ shared/src/main/java/webSocketMessages/serverMessages/ErrorMessage.java
@@ -0,0 +1,13 @@
+package webSocketMessages.serverMessages;
+
+public class ErrorMessage extends ServerMessage{
+    String message;
+    public ErrorMessage(String message) {
+        super(ServerMessageType.ERROR);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
