a16b39e410018c9bd3f43491f69e336125ad7268
Miles Spencer <milesspencer35@gmail.com>
2024-04-10 21:11:50 -0600

    Add games map to ConnectionManager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -10,14 +10,23 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     // ConcurrentHashMap is multi thread safe
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<String, Integer> games = new ConcurrentHashMap<>();
 
-    public void add(String authToken, Session session) {
+    public void addConnection(String authToken, Session session) {
         var connection = new Connection(authToken, session);
         connections.put(authToken, connection);
     }
 
-    public void remove(String authToken) {
+    public void removeConnection(String authToken) {
         connections.remove(authToken);
+    }
+
+    public void addPlayer(String authToken, Integer gameID) {
+        games.put(authToken, gameID);
+    }
+
+    public void removePlayer(String authToken, Integer gameID) {
+        games.remove(authToken);
     }
 
     public void broadcast(String excludeAuthToken, ServerMessage serverMessage) throws IOException {
